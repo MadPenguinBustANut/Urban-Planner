0bd29e52b0d60bdab5f024182f42150c074ba387
mystero1 <mysterostefano@hotmail.it>
2017-11-22 16:53:23 +0100

    PROVA SUL MIO PC FISSO DIMMI SE FUNZIONA PLEASE

--- src/exec/Lelloworld.java
+++ src/exec/Lelloworld.java
@@ -1,10 +1,9 @@
 package exec;
 
 public class Lelloworld {
- private int i=2;
- private int j=3;
- public Lelloworld() {
-	 System.out.println("negro" + j + i);
+	public String minchia= "WOAH QUESTO PROGRAMMA PARLA";
+	public Lelloworld() {
+	 System.out.println("MA NIGGA");
  }
 
 }
