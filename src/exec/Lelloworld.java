485758e7f276f7132385d4e095000f8627fb0599
mystero1 <33893435+mystero1@users.noreply.github.com>
2017-11-22 17:05:24 +0100

    Update Lelloworld.java

--- src/exec/Lelloworld.java
+++ src/exec/Lelloworld.java
@@ -2,6 +2,7 @@ package exec;
 
 public class Lelloworld {
 	public String minchia= "WOAH QUESTO PROGRAMMA PARLA";
+	public String trancio = "Tie questo lo ho scritto sul browser";
 	public Lelloworld() {
 	 System.out.println("MA NIGGA");
  }
