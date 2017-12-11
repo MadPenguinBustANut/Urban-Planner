7124dada65277173b3c446c543611de5eb6a3cb5
Kinglash <vitolanaro@gmail.com>
2017-12-11 13:09:16 +0100

    Iniziati il frame e il pannello per la modifica

---
+++ src/gui/FrameLotto.java
@@ -0,0 +1,24 @@
+package gui;
+
+import javax.swing.JFrame;
+
+import centrourbano.Lotti;
+
+public class FrameLotto extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	private Lotti rifer;
+	
+	
+	
+	public FrameLotto(Lotti e) {
+		super("Lotto");
+		rifer = e;
+		
+		
+		
+		
+		
+	}
+	
+}
