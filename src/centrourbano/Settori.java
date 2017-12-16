6ff6df69c2733a1137db47e2ce1cb4165fa13cbc
mystero1 <mysterostefano@hotmail.it>
2017-12-16 16:44:55 +0100

    Sto aggiungendo i commenti

--- src/centrourbano/Settori.java
+++ src/centrourbano/Settori.java
@@ -19,8 +19,11 @@ public class Settori implements Serializable{
 	
 	
 	/**
-	 * Calcola il prezzo del lotto
-	 * @param settore
+	 * Calcola il prezzo del lotto, questo metodo si applica solo per gli edifici privati
+	 * essendo gli unici con un valore e elimina il lotto essendo stato venduto
+	 * @param X Coordinata X, che insieme alla Y indicano che edificio vendere
+	 * @param Y Coordinata Y, che insieme alla X indicano che edificio vendere
+	 * @return	Ritorna il prezzo di vendita dell'edificio
 	 */
 	public int vendiEdificio(int X, int Y) {
 		Lotti vend = getLotto(X, Y);
@@ -37,7 +40,7 @@ public class Settori implements Serializable{
 	 * -Edificio pubblico: Incrementa di 1 il valore del settore
 	 * -Strada: Trova i lotti adiacenti nelle 8 direzioni e aumenta il loro valore di 1
 	 * -Edificio privato: Controlla i lotti adiacenti in cerca di strade e aumenta per ogni strada il valore di 1
-	 * @param Nuovo
+	 * @param Nuovo Il Lotto che va inserito
 	 */
 	public void addLotto(Lotti Nuovo, int X, int Y) {
 		if(Nuovo.getTip() == STRADA)
@@ -59,8 +62,8 @@ public class Settori implements Serializable{
 	 * 
 	 * 
 	 * Nel caso in cui il lotto sia gi� libero lancia un eccezione di tipo "LottoLibero"
-	 * @param X
-	 * @param Y
+	 * @param X Coordinata X, che insieme alla Y indicano che edificio rimuovere
+	 * @param Y Coordinata Y, che insieme alla X indicano che edificio rimuovere
 	 */
 	
 	public void rmLotto(int X, int Y) {
@@ -77,15 +80,22 @@ public class Settori implements Serializable{
 	
 	/**
 	 * Cambia l'edificio selezionato con un altro aggiornando gli effetti
-	 * @param X
-	 * @param Y
-	 * @throws LottoLibero
+	 * @param X Coordinata X, che insieme alla Y indicano che edificio cambiare
+	 * @param Y Coordinata Y, che insieme alla X indicano che edificio cambiare
+	 * @throws LottoLibero Nel caso il lotto sia gia libero lancia una eccezzione
 	 */
 	
 	public void cgEdificio(Lotti nuovo,int X, int Y) {
 		rmLotto(X, Y);
 		addLotto(nuovo, X , Y);
 	}
+	
+	/**
+	 * Restituisce il lotto in posizione X, Y
+	 * @param X Coordinata X, che insieme alla Y indicano che edificio restituire
+	 * @param Y Coordinata Y, che insieme alla X indicano che edificio restituire
+	 * @return
+	 */
 	
 	public Lotti getLotto(int X, int Y) {
 		Lotti io = lista[X][Y]; 		
@@ -96,30 +106,31 @@ public class Settori implements Serializable{
 	
 	/**
 	 * Aggiunge una strada alla posizione indicata da X e Y e aumenta di uno 
-	 * il valore di tutti i lotti adiacenti
-	 * @param Nuovo
-	 * @param X
-	 * @param Y
+	 * il valore di tutti i lotti adiacenti interni al settore (per quelli esterni se ne occupa il 
+	 * metodo di CentroUrbano, perfavore usare quello e non questo nel caso si debba aggiungere una strada)
+	 * @param Nuovo Il lotto da inserire
+	 * @param X Coordinata X, che insieme alla Y indicano la posizione della strada
+	 * @param Y Coordinata Y, che insieme alla X indicano la posizione della strada
 	 */
 	private void addStrada(Lotti Nuovo, int X, int Y) {
 			lista[X][Y] = Nuovo;
 			
-			if((Y - 1) >= 0)
+			if((Y - 1) > -1)
 				addOne(X,Y-1);
-			if((Y + 1) <= MAX_Y )
+			if((Y + 1) < MAX_Y )
 				addOne(X,Y+1);
-			if((X - 1) >= 0) {
+			if((X - 1) > -1) {
 				addOne(X-1, Y);
-				if((Y-1) >= 0)
+				if((Y-1) > -1)
 					addOne(X-1, Y-1);
-				if((Y + 1) <= MAX_Y)
+				if((Y + 1) < MAX_Y)
 					addOne(X-1, Y+1);
 			}
-			if((X + 1) <= MAX_X) {
+			if((X + 1) < MAX_X) {
 				addOne(X+1, Y);
-				if((Y-1) >= 0)
+				if((Y-1) > -1)
 					addOne(X+1, Y-1);
-				if((Y+1) <= MAX_Y)
+				if((Y+1) < MAX_Y)
 					addOne(X+1,Y+1); 
 			}
 	}
@@ -128,9 +139,9 @@ public class Settori implements Serializable{
 	
 	/**
 	 * Aggiunge un edificio pubblico e aumenta il valore del settore di 1
-	 * @param Nuovo
-	 * @param X
-	 * @param Y
+	 * @param Nuovo Il lotto da aggiungere
+	 * @param X Coordinata X, che insieme alla Y indicano in che posizione aggiungere il lotto
+	 * @param Y Coordinata Y, che insieme alla X indicano in che posizione aggiungere il lotto
 	 */
 	private void addepub(Lotti Nuovo, int X, int Y) {
 		lista[X][Y] = Nuovo;
@@ -148,22 +159,22 @@ public class Settori implements Serializable{
 	 */
 	private void rmStrada(int X, int Y) {
 		lista[X][Y].setEdificio(null);
-		if((Y - 1) >= 0)
+		if((Y - 1) > -1)
 			subOne(X,Y-1);
-		if((Y + 1) <= MAX_Y )
+		if((Y + 1) < MAX_Y )
 			subOne(X,Y+1);
-		if((X - 1) >= 0) {
+		if((X - 1) > -1) {
 			subOne(X-1, Y);
-			if((Y-1) >= 0)
+			if((Y-1) > -1)
 				subOne(X-1, Y-1);
-			if((Y + 1) <= MAX_Y)
+			if((Y + 1) < MAX_Y)
 				subOne(X-1, Y+1);
 		}
-		if((X + 1) <= MAX_X) {
+		if((X + 1) < MAX_X) {
 			subOne(X+1, Y);
-			if((Y-1) >= 0)
+			if((Y-1) > -1)
 				subOne(X+1, Y-1);
-			if((Y+1) <= MAX_Y)
+			if((Y+1) < MAX_Y)
 				subOne(X+1,Y+1); 
 			}
 		}
@@ -196,8 +207,8 @@ public class Settori implements Serializable{
 	
 	public int calcolaLottiLiberi() {
 		int lottiLiberi = 0;
-		for(int i = 0; i <= MAX_X ; i++ ) {
-			for(int j=0;j <= MAX_Y; j++) {
+		for(int i = 0; i < MAX_X ; i++ ) {
+			for(int j=0;j < MAX_Y; j++) {
 				if(lista[i][j].getTip() == LIBERO)
 					lottiLiberi++;
 			}
@@ -209,8 +220,8 @@ public class Settori implements Serializable{
 	
 	public int calcolaStrade() {
 		int strade = 0;
-		for(int i = 0; i <= MAX_X ; i++ ) {
-			for(int j=0;j <= MAX_Y; j++) {
+		for(int i = 0; i < MAX_X ; i++ ) {
+			for(int j=0;j < MAX_Y; j++) {
 				if(lista[i][j].getTip() == STRADA)
 					strade++;
 			}
@@ -223,8 +234,8 @@ public class Settori implements Serializable{
 	
 	public int calcolaLottiPubblici() {
 		int lottiPubblici = 0;
-		for(int i = 0; i <= MAX_X ; i++ ) {
-			for(int j=0;j <= MAX_Y; j++) {
+		for(int i = 0; i < MAX_X ; i++ ) {
+			for(int j=0;j < MAX_Y; j++) {
 				if(lista[i][j].getTip() == EPUB)
 					lottiPubblici++;
 			}
@@ -237,8 +248,8 @@ public class Settori implements Serializable{
 
 	public int calcolaLottiPrivati() {
 		int lottiPrivati = 0;
-		for(int i = 0; i <= MAX_X ; i++ ) {
-			for(int j=0;j <= MAX_Y; j++) {
+		for(int i = 0; i < MAX_X ; i++ ) {
+			for(int j=0;j < MAX_Y; j++) {
 				if(lista[i][j].getTip() == EPRIV)
 					lottiPrivati++;
 			}
@@ -267,8 +278,8 @@ public class Settori implements Serializable{
 
 
 
-	private static final int MAX_X = 2;
-	private static final int MAX_Y = 4;
+	private static final int MAX_X = 3;
+	private static final int MAX_Y = 5;
 	private static final int LIBERO = 0;
 	private static final int STRADA = 1;
 	private static final int EPUB = 2;
