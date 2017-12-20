3eff2fa8ee05f838033c18b5c00bc8d91c79822a
T2049 <redtiziana@gmail.com>
2017-12-20 18:35:02 +0100

    pulsanti attivi

--- src/gui/FrameModifica.java
+++ src/gui/FrameModifica.java
@@ -2,7 +2,10 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -21,11 +24,22 @@ public class FrameModifica extends JFrame{
 		JPanel io = new JPanel(new BorderLayout());
 		JPanel io2 = new JPanel();
 		PannelloSettore x = new PannelloSettore();
-		PannelloPulsanti PPul = new PannelloPulsanti();
+		JPanel PPul = new JPanel(new GridLayout(1,2));
+		
+		JButton modStrada = new JButton("crea strada");
+		JButton modSettore = new JButton("modifica settore");
+		PPul.add(modStrada);
+		PPul.add(modSettore);
+		
+		modStrada.addActionListener(new ButtonListener());
+		modSettore.addActionListener(new ButtonListener());
+		
 		io.add(x, BorderLayout.CENTER);
 		io2.add(PPul);
 		add(io, BorderLayout.CENTER);
 		add(io2, BorderLayout.SOUTH);
+		
+		
 		
 		setTitle("Modifica");
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -33,5 +47,27 @@ public class FrameModifica extends JFrame{
 		setVisible(true);
 	}
 	
+	public class ButtonListener implements ActionListener {
+
+		public void actionPerformed(ActionEvent e) {
+			JButton rif = (JButton) e.getSource();
+			if(rif.getText().equalsIgnoreCase("crea strada")) {
+				JFrame nuovoFrame = new JFrame ();
+				CreaStrada nuovaS = new CreaStrada ();
+
+				nuovoFrame.add(nuovaS);
+				setVisible(true);
+				setSize(720, 560);
+				setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+			}
+			else if(rif.getText().equalsIgnoreCase("modifica settore")) {
+				FrameModificaLotto unLotto = new FrameModificaLotto();
+			}
+		}
+			
+			
+				
+		}
+		
 	
 }
