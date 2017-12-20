5a69f8b41c0ad2dc9910f82a6d4b38653c9a8864
T2049 <redtiziana@gmail.com>
2017-12-20 22:28:10 +0100

    passolecoordinate?

--- src/gui/FrameModifica.java
+++ src/gui/FrameModifica.java
@@ -5,6 +5,7 @@ import java.awt.GridLayout;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -58,36 +59,49 @@ public class FrameModifica extends JFrame{
 		setVisible(true);
 	}
 	
-	public class ButtonListener implements ActionListener {
-
-		public void actionPerformed(ActionEvent e) {
-			JButton rif = (JButton) e.getSource();
-			if(rif.getText().equalsIgnoreCase("crea strada")) {
-				
-				Point newP = new Point(settX, settY);				
-				
-				JFrame nuovoFrame = new JFrame ();
-				CreaStrada nuovaS = new CreaStrada (newP, rifer.lista[settX][settY], rifer);
-				nuovoFrame.add(nuovaS);
-				nuovoFrame.setSize(700, 700);
-				nuovoFrame.setVisible(true);
-				nuovoFrame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		public void mouseClicked(MouseEvent e) {
+			int a = e.getX();
+			int b = e.getY();
 			
+			if(a > 3 & b > 3) {
+				if(a <= 3+(5*10*5) & b <= 3+(5*10*3)) {
+					
+					 settX = a/(10);
+					 settY = b/(10);
+				
+				
 			}
-			
-			else if(rif.getText().equalsIgnoreCase("modifica settore")) {
-				Point newP = new Point(settX, settY);
-				JFrame nuovoFrame = new JFrame ();
-				PannelloVModifica VMod = new PannelloVModifica(rifer.lista[settX][settY], rifer, newP);
+		}	
+		}
+		
+		public class ButtonListener implements ActionListener {
+
+			public void actionPerformed(ActionEvent e) {
 				
+				JButton rif = (JButton) e.getSource();
+				if(rif.getText().equalsIgnoreCase("crea strada")) {
+					
+					Point newP = new Point(settX, settY);				
+					
+					JFrame nuovoFrame = new JFrame ();
+					CreaStrada nuovaS = new CreaStrada (newP, rifer.lista[settX][settY], rifer);
+					nuovoFrame.add(nuovaS);
+					nuovoFrame.setSize(700, 700);
+					nuovoFrame.setVisible(true);
+					nuovoFrame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+				}
 				
-				nuovoFrame.add(VMod);
-				nuovoFrame.setVisible(true);
-				nuovoFrame.setSize(700,700);
-				nuovoFrame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+				else if(rif.getText().equalsIgnoreCase("modifica settore")) {
+					Point newP = new Point(settX, settY);
+					JFrame nuovoFrame = new JFrame ();
+					PannelloVModifica VMod = new PannelloVModifica(rifer.lista[settX][settY], rifer, newP);
+					
+					
+					nuovoFrame.add(VMod);
+					nuovoFrame.setVisible(true);
+					nuovoFrame.setSize(700,700);
+					nuovoFrame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+				}	
 			}
 		}
-			
 	}
-		
-}
