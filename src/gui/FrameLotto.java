15b1a348f8225c1cde83aaeb8f6390048d91312f
MadPenguinBustANut <megamadpenguin@gmail.com>
2017-12-21 16:15:02 +0100

    aaa
    aaa

--- src/gui/FrameLotto.java
+++ src/gui/FrameLotto.java
@@ -163,7 +163,7 @@ public class FrameLotto extends JFrame {
 		if(removeButton.isEnabled()) {
 			JFrame temp= new JFrame("Errore");
 			temp.add(new JLabel("Demolisci prima di costruire."));
-			temp.setDefaultCloseOperation(EXIT_ON_CLOSE);
+			temp.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 			temp.setSize(200,110);
 			temp.setVisible(true);
 		}
