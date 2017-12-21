03ad24ad348019bdb4a67bfed88988c6ebe45dd0
MadPenguinBustANut <megamadpenguin@gmail.com>
2017-12-21 23:17:24 +0100

    aaaaaa
    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa fuckkk

--- src/gui/StartFrame.java
+++ src/gui/StartFrame.java
@@ -4,16 +4,14 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
+
 import javax.swing.JPanel;
 
 import ascoltatori.StartFrameListener;
